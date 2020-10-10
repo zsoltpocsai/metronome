133168f7c7d04dbcd4a6ec31b9250702d3563c4a
Zsolt Pocsai <pzsolt1214@gmail.com>
2020-10-10 16:54:42 +0200

    added startStopButton

--- src/stuff/metronome/view/StartStopButton.java
+++ src/stuff/metronome/view/StartStopButton.java
@@ -2,23 +2,55 @@ package stuff.metronome.view;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.text.Font;
 
 public class StartStopButton extends Button {
 	
+	private boolean isSetOnStart;
+	private EventHandler<ActionEvent> onStart;
+	private EventHandler<ActionEvent> onStop;
+	
 	public StartStopButton() {
 		super();
+		isSetOnStart = true;
 		this.setText("Start");
+		onStart = new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent e) {}
+		};
+		onStop = new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent e) {}
+		};
 		this.setOnAction(new EventHandler<ActionEvent>() {
-			
 			@Override
 			public void handle(ActionEvent e) {
-				System.out.println("Action event: " + e);
+				if (isSetOnStart) {
+					onStart.handle(e);
+				} else {
+					onStop.handle(e);
+				}
+				isSetOnStart = !isSetOnStart;
+				updateState();
 			}
 		});
+		this.setPadding(new Insets(10.0, 30.0, 10.0, 30.0));
+		this.setFont(new Font(26.0));
 	}
 	
-	public void setOnStart() {
-		
+	public void setOnStart(EventHandler<ActionEvent> handler) {
+		this.onStart = handler;
+	}
+	
+	public void setOnStop(EventHandler<ActionEvent> handler) {
+		this.onStop = handler;
+	}
+	
+	private void updateState() {
+		if (isSetOnStart) {
+			this.setText("Start");
+		} else {
+			this.setText("Stop");
+		}
 	}
 }
